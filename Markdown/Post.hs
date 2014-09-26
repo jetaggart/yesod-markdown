38ba850c25e823fc577d08bc72a1d1e2c0f76099
Jeff Taggart <jeff@jetaggart.com>
2014-09-26 16:25:52 -0600

    Got posts to list
    It's really ugly :(

--- Markdown/Post.hs
+++ Markdown/Post.hs
@@ -1,9 +1,38 @@
 module Markdown.Post where
 
 import Prelude
+import System.Directory
+import Data.List
+import Data.String
+
 import Markdown.Types
 
-data Post = Post PageName
+
+
+data Post = Post PageName deriving (Show)
 
 findPosts :: IO [Post]
-findPosts = return [(Post "name")]
+findPosts = do
+  directory <- getCurrentDirectory
+  let postDirectory = directory ++ "/posts"
+
+  posts <- getDirectoryContents postDirectory
+
+  return $ (map createPost . filter markdownFile) posts
+  where
+    createPost p = Post $ (splitName . partitionString) p
+    splitName (fileName, _) = fileName
+    partitionString p = splitAt (index p) p
+    index p = findIndexOf $ findIndex (\a -> a == '.') p
+
+    findIndexOf (Just index) = index
+    findIndexOf Nothing = 0
+                   
+
+
+    markdownFile = isInfixOf ".md"
+
+                             
+  
+  
+
